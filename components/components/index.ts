d36e0088a95cde116f9ee2d2468425cd2c3eac86
l_asrdn <ansorudinlabib1@gmail.com>
2023-05-15 14:09:02 +0700

    feat: slicing navbar

---
+++ components/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Container } from "./Container/Container";
+export { default as Search } from "./Search/Search";
+export { default as UserMenu } from "./UserMenu/UserMenu";
+export { default as Avatar } from "./Avatar/Avatar";
---
+++ components/ui/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import { Container, Search, UserMenu } from "@/components/components";
+import Logo from "./Logo";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
